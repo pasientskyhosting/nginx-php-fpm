f091177ad1ecadf212972d08926b2f783faccde7
Andreas Kruger <ak@pasientsky.no>
2017-01-06 09:51:01 +0100

    Removed non used things

--- scripts/start.sh
+++ scripts/start.sh
@@ -53,54 +53,8 @@ if [ ! -d "/var/www/html/.git" ]; then
  fi
 fi
 
-# Enable custom nginx config files if they exist
-if [ -f /var/www/html/conf/nginx/nginx-site.conf ]; then
-  cp /var/www/html/conf/nginx/nginx-site.conf /etc/nginx/sites-available/default.conf
-fi
-
-# Display PHP error's or not
-if [[ "$ERRORS" != "1" ]] ; then
- echo php_flag[display_errors] = off >> $fpm_conf
-else
- echo php_flag[display_errors] = on >> $fpm_conf
-fi
-
-# Display Version Details or not
-if [[ "$HIDE_NGINX_HEADERS" == "0" ]] ; then
- sed -i "s/server_tokens off;/server_tokens on;/g" /etc/nginx/nginx.conf
-else
- sed -i "s/expose_php = On/expose_php = Off/g" $php_conf
-fi
-
-# Increase the memory_limit
-if [ ! -z "$PHP_MEM_LIMIT" ]; then
- sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEM_LIMIT}M/g" $php_conf
-fi
-
-# Increase the post_max_size
-if [ ! -z "$PHP_POST_MAX_SIZE" ]; then
- sed -i "s/post_max_size = 100M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" $php_conf
-fi
-
-# Increase the upload_max_filesize
-if [ ! -z "$PHP_UPLOAD_MAX_FILESIZE" ]; then
- sed -i "s/upload_max_filesize = 100M/upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M/g" $php_conf
-fi
-
 # Always chown webroot for better mounting
 chown -Rf nginx.nginx /var/www/html
-
-# Run custom scripts
-if [[ "$RUN_SCRIPTS" == "1" ]] ; then
-  if [ -d "/var/www/html/scripts/" ]; then
-    # make scripts executable incase they aren't
-    chmod -Rf 750 /var/www/html/scripts/*
-    # run scripts in number order
-    for i in `ls /var/www/html/scripts/`; do /var/www/html/scripts/$i ; done
-  else
-    echo "Can't find script directory"
-  fi
-fi
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
