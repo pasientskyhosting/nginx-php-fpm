d90957421d378daac6c430bf7cd6eec052b14fc9
Andreas Kruger <ak@pasientsky.no>
2017-03-14 16:00:09 +0100

    Updated error pages and added build id fix to parameters

--- scripts/start.sh
+++ scripts/start.sh
@@ -83,6 +83,10 @@ if [ ! -d "/var/www/html/.git" ]; then
     fi
 fi
 
+if [ -f /var/www/html/app/config/parameters.yml ]; then
+    sed -i "s/{{build_id}}/$PS_BUILD_ID/" /var/www/html/app/config/parameters.yml
+fi
+
 # Composer
 if [ -f /var/www/html/composer.json ]; then
 
@@ -101,6 +105,8 @@ EOF
     /usr/bin/composer install --no-interaction --no-dev --optimize-autoloader
     php app/console cache:clear --env=prod
 fi
+
+
 
 # Always chown webroot for better mounting
 chown -R nginx:nginx /var/www/html
