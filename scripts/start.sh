6a91e78271e61ceaf1b99454c54e2d7aca08e04f
Maurcio Burgos Herrera <mburgosherrera@gmail.com>
2017-04-18 13:45:31 +0200

    Update start.sh
    Leave chown to setup.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 /setup.sh
 
 # Make sure permissions is always right
-chown -R nginx:nginx /var/www/html
+# chown -R nginx:nginx /var/www/html
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
