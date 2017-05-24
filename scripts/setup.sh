df9b02be1e23501512c053930f6503522cc6745e
Andreas Kruger <ak@pasientsky.no>
2017-05-24 14:44:20 +0200

    Fixed double load of newrelic

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,8 +12,12 @@ if [ ! -z "$NEW_RELIC_LICENSE_KEY" ]; then
     nrsysmond-config --set license_key=${NEW_RELIC_LICENSE_KEY} || exit 1
     echo -e "\n[program:nrsysmond]\ncommand=nrsysmond -c /etc/newrelic/nrsysmond.cfg -l /dev/stdout -f\nautostart=true\nautorestart=true\npriority=0\nstdout_events_enabled=true\nstderr_events_enabled=true\nstdout_logfile=/dev/stdout\nstdout_logfile_maxbytes=0\nstderr_logfile=/dev/stderr\nstderr_logfile_maxbytes=0" >> /etc/supervisord.conf
 
-    sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"$PS_DEPLOYMENT_DATACENTER-cluster1-$PS_ENVIRONMENT-$PS_APPLICATION\"|" /etc/php/7.1/fpm/conf.d/20-newrelic.ini
-    sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"$PS_DEPLOYMENT_DATACENTER-cluster1-$PS_ENVIRONMENT-$PS_APPLICATION\"|" /etc/php/7.1/cli/conf.d/20-newrelic.ini
+    if [ -f /etc/php/7.1/fpm/conf.d/20-newrelic.ini ]; then
+        rm -rf /etc/php/7.1/fpm/conf.d/20-newrelic.ini
+    fi
+    if [ -f /etc/php/7.1/cli/conf.d/20-newrelic.ini ]; then
+        rm -rf /etc/php/7.1/cli/conf.d/20-newrelic.ini
+    fi
 
     sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"$PS_DEPLOYMENT_DATACENTER-cluster1-$PS_ENVIRONMENT-$PS_APPLICATION\"|" /etc/php/7.1/fpm/conf.d/newrelic.ini
     sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"$PS_DEPLOYMENT_DATACENTER-cluster1-$PS_ENVIRONMENT-$PS_APPLICATION\"|" /etc/php/7.1/cli/conf.d/newrelic.ini
@@ -25,6 +29,12 @@ else
     fi
     if [ -f /etc/php/7.1/cli/conf.d/20-newrelic.ini ]; then
         rm -rf /etc/php/7.1/cli/conf.d/20-newrelic.ini
+    fi
+    if [ -f /etc/php/7.1/fpm/conf.d/newrelic.ini ]; then
+        rm -rf /etc/php/7.1/fpm/conf.d/newrelic.ini
+    fi
+    if [ -f /etc/php/7.1/cli/conf.d/newrelic.ini ]; then
+        rm -rf /etc/php/7.1/cli/conf.d/newrelic.ini
     fi
     /etc/init.d/newrelic-daemon stop
 fi
